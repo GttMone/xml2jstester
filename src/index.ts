441512bf200fb9f06e9138355a757a8a6d8617f6
Vito Gos <davidgospodinov10@gmail.com>
2024-12-10 23:26:25 +0200

    .

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const feed = 'https://www.colibri.bg/xml/colibri_books.php';
+
+import axios from 'axios';
+import fs from 'node:fs/promises';
+import { parseStringPromise as parseXMLFeed } from 'xml2js';
+
+const res = await axios.get(feed, { auth: { username: 'guest', password: 'xmltoguest' } });
+const parsedFeed = await parseXMLFeed(res.data, { async: true, explicitArray: false });
+await fs.writeFile('output.json', JSON.stringify(parsedFeed, null, 2));
